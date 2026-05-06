58b0023f434ab2ddf434f8467e41c7a3bbf2fadb
Siddharth <connectwithsid95@gmail.com>
2026-05-07 00:46:19 +0530

    half baked

---
+++ logistic_regression.py
@@ -0,0 +1,54 @@
+import math 
+
+class Logistic_regression:
+    def __init__(self, lr = 0.5 , epochs = 500):
+        self.lr = lr
+        self.epochs = epochs
+        self.w = None
+        self.b = 0
+
+    def sigmoid(z):
+        return 1 / (1 + math.exp(-z))   
+
+    def predict_single(self,x):
+        z = 0
+        #z=w1​x1 ​+w2​x2 ​+⋯+ wd​xd ​+bias
+        for j in range(len(self.w)):
+            z += x[j] * self.w[j]
+        z += self.b
+        return self.sigmoid(z)
+    
+    def predict_proba(self,X):
+        return [self.predict_single(x) for x in X] #x is a list of x1,x2 pairs
+
+    def loss(self,y,y_preds):
+        total = 0
+        n = len(y) #number of samples 
+        for i in range(n):
+            p = y_preds[i]
+            p = max(min(p, 1 - 1e-10), 1e-10) # epsilon value to avoid log(0)
+            if y[i] == 1:
+                total += -math.log(p)
+            else:
+                total += -math.log(1 - p)
+        return total / n
+
+
+#creating a dataset 
+import random
+random.seed(42)
+X = []
+y = []
+for _ in range(100):
+    x1 = random.random()
+    x2 = random.random()
+    X.append([x1,x2])
+    
+    # decision rule (ground truth)
+    if 2*x1 + x2 > 1.2:
+        y.append(1)
+    else:
+        y.append(0)
+#print(X)    #shape should be (100,2)    
+#print(y)    #shape should be (100,)   
+    
